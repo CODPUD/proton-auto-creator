071370f61adc55082526d93ab21e8a7b6772fc38
CODPUD <jalol.kham.2401@gmail.com>
2020-11-29 03:02:32 +0100

    OOP commit

--- main.py
+++ main.py
@@ -3,25 +3,41 @@ import time
 import random
 import string
 
-def generate_string(n):
-    letters = string.ascii_letters + string.digits
-    result = ''.join([random.choice(letters) for i in range(n)])
-    return result
+class ProtonMail():
+    def __init__(self):
+        options = webdriver.FirefoxOptions()
+        options.set_preference('dom.webdriver.enabled', False)
+        self.driver = webdriver.Firefox(options=options)
+        self.driver.maximize_window()
 
-driver = webdriver.Firefox()
-driver.get('https://mail.protonmail.com/create/new?language=en')
-time.sleep(3)
-driver.switch_to.frame(1)
-time.sleep(1)
-login = generate_string(15)
-driver.find_element_by_xpath('/html/body/div/div/div/div/input').send_keys(login)
-time.sleep(1)
-driver.switch_to.default_content()
-time.sleep(1)
-password = generate_string(15)
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[1]/form/div[1]/div[2]/div[1]/div/input').send_keys(password)
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[1]/form/div[1]/div[2]/div[2]/div/input').send_keys(password)
-time.sleep(1)
-driver.switch_to.frame(0)
-time.sleep(1)
-driver.find_element_by_xpath('/html/body/div/div/footer/button').click()
+    def generate_string(self, n):
+        letters = string.ascii_letters + string.digits
+        result = ''.join([random.choice(letters) for i in range(n)])
+        return result
+
+    def fillForm(self):
+        self.driver.get('https://mail.protonmail.com/create/new?language=en')
+        time.sleep(3)
+        self.driver.switch_to.frame(1)
+        time.sleep(1)
+        login = self.generate_string(15)
+        self.driver.find_element_by_xpath('/html/body/div/div/div/div/input').send_keys(login)
+        time.sleep(1)
+        self.driver.switch_to.default_content()
+        time.sleep(1)
+        password = self.generate_string(15)
+        self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div/div/div/div[1]/form/div[1]/div[2]/div[1]/div/input').send_keys(password)
+        self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div/div/div/div[1]/form/div[1]/div[2]/div[2]/div/input').send_keys(password)
+        time.sleep(1)
+        self.driver.switch_to.frame(0)
+        time.sleep(1)
+        self.driver.find_element_by_xpath('/html/body/div/div/footer/button').click()
+
+    def quit(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    root = ProtonMail()
+    root.fillForm()
